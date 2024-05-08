90489a0b8cb8b9324e1bdec53e74fde09b4ca97b
Luis Roberto Rivera Gonzalez <181G0258@rcarbonifera.tecnm.mx>
2024-05-07 20:14:18 -0600

    Implemente JWT al swagger

---
+++ U3ActRegistroDeActividadesApi/Controllers/DepartamentosController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace U3ActRegistroDeActividadesApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartamentosController : ControllerBase
+    {
+    }
+}
--- U3ActRegistroDeActividadesApi/Program.cs
+++ U3ActRegistroDeActividadesApi/Program.cs
@@ -1,19 +1,45 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using U3ActRegistroDeActividadesApi.Models.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
+#region Servicios
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+#region Agregar Swagger con JWT
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "RegistroActividadesApi", Version = "v1" });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "Ingresa el token recibido por el login",
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer"
 
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
+#endregion
+#endregion
 #region Base de datos
 var Db = builder.Configuration.GetConnectionString("DbConnectionString");
 builder.Services.AddMySql<ItesrcneActividadesContext>(Db, ServerVersion.AutoDetect(Db));
 #endregion
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
