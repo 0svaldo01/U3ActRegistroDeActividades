4d969c74258379f38afa0b39e8e395e46d876dbf
Luis Roberto Rivera Gonzalez <181G0258@rcarbonifera.tecnm.mx>
2024-05-07 20:09:56 -0600

    Cree un repositorio Generico

---
+++ U3ActRegistroDeActividadesApi/Repositories/GenericRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using U3ActRegistroDeActividadesApi.Models.Entities;
+
+namespace U3ActRegistroDeActividadesApi.Repositories
+{
+    public class GenericRepository<T>(ItesrcneActividadesContext Context) where T : class
+    {
+        public virtual DbSet<T> GetAll()
+        {
+            return Context.Set<T>();
+        }
+        public virtual T? GetById(int id)
+        {
+            return Context.Find<T>(id);
+        }
+        public virtual void Insert(T entidad)
+        {
+            Context.Add(entidad);
+            Context.SaveChanges();
+        }
+        public virtual void Update(T entidad)
+        {
+            Context.Update(entidad);
+            Context.SaveChanges();
+        }
+        public virtual void Delete(T entidad)
+        {
+            Context.Remove(entidad);
+            Context.SaveChanges();
+        }
+    }
+}
