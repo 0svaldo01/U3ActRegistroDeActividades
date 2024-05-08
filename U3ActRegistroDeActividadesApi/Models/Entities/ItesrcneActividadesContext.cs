823f11cf55a2221c5e48ab53a2b1def9a07c1dae
Luis Roberto Rivera Gonzalez <181G0258@rcarbonifera.tecnm.mx>
2024-05-07 20:05:48 -0600

    Asegure la conexion a la base de datos

--- U3ActRegistroDeActividadesApi/Models/Entities/ItesrcneActividadesContext.cs
+++ U3ActRegistroDeActividadesApi/Models/Entities/ItesrcneActividadesContext.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
-using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace U3ActRegistroDeActividadesApi.Models.Entities;
 
@@ -19,10 +16,6 @@ public partial class ItesrcneActividadesContext : DbContext
     public virtual DbSet<Actividades> Actividades { get; set; }
 
     public virtual DbSet<Departamentos> Departamentos { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=204.93.216.11;database=itesrcne_actividades;user=itesrcne_deptos;password=sistemaregistrotec24", Microsoft.EntityFrameworkCore.ServerVersion.Parse("10.3.29-mariadb"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
