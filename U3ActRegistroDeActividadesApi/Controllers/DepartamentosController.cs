340afb24f509bd8491d221ec0fa8cd03e251c429
Luis Roberto Rivera Gonzalez <181G0258@rcarbonifera.tecnm.mx>
2024-05-08 00:14:26 -0600

    solucione problemas con los repositorios

--- U3ActRegistroDeActividadesApi/Controllers/DepartamentosController.cs
+++ U3ActRegistroDeActividadesApi/Controllers/DepartamentosController.cs
@@ -1,16 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using U3ActRegistroDeActividadesApi.Repositories;
 
 namespace U3ActRegistroDeActividadesApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class DepartamentosController(Repositories.DepartamentosRepository departamentosRepository) : ControllerBase
+    public class DepartamentosController(DepartamentosRepository departamentosRepository) : ControllerBase
     {
         [HttpGet("/")]
         public IActionResult GetDepartamentos()
         {
-            var departamentos = departamentosRepository.GetDepartamentos();
-            return departamentos != null ? Ok(departamentos) : NotFound("No hay departamentos existentes");
+            var deptos = departamentosRepository.GetDepartamentos();
+            if (deptos != null)
+            {
+                return Ok(deptos);
+            }
+            return NotFound("No se han encontrado departamentos");
         }
     }
 }
--- U3ActRegistroDeActividadesApi/Program.cs
+++ U3ActRegistroDeActividadesApi/Program.cs
@@ -3,11 +3,9 @@ using Microsoft.OpenApi.Models;
 using System.Text.Json.Serialization;
 using U3ActRegistroDeActividadesApi.Models.Entities;
 using U3ActRegistroDeActividadesApi.Repositories;
-
 var builder = WebApplication.CreateBuilder(args);
-#region Servicios                                                                        
-builder.Services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles)
-    .AddJsonOptions(x => x.JsonSerializerOptions.MaxDepth = 6);
+#region Servicios
+builder.Services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 #region Agregar Swagger con JWT
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
@@ -36,28 +34,25 @@ builder.Services.AddSwaggerGen(c =>
     });
 });
 #endregion
+#region Transient`s
+builder.Services.AddTransient<DepartamentosRepository>();
+#endregion
 #endregion
 #region Base de datos
 var Db = builder.Configuration.GetConnectionString("DbConnectionString");
 builder.Services.AddMySql<ItesrcneActividadesContext>(Db, ServerVersion.AutoDetect(Db));
 #endregion
-#region
-builder.Services.AddTransient<DepartamentosRepository>();
-
-#endregion
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
+#region Implementacion Swagger
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+#endregion
+#region Configuracion
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
+#endregion
