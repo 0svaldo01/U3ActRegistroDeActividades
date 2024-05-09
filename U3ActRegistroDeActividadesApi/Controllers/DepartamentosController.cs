9db748b62bcb6aff04fd1fe1ac40c9470859aaa8
Luis Roberto Rivera Gonzalez <181G0258@rcarbonifera.tecnm.mx>
2024-05-09 17:01:33 -0600

    Agregue el metodo Delete y un ejemplo de validacion con fluentValidation

--- U3ActRegistroDeActividadesApi/Controllers/DepartamentosController.cs
+++ U3ActRegistroDeActividadesApi/Controllers/DepartamentosController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using U3ActRegistroDeActividadesApi.Models.DTOs;
+using U3ActRegistroDeActividadesApi.Models.Validators;
 using U3ActRegistroDeActividadesApi.Repositories;
 
 namespace U3ActRegistroDeActividadesApi.Controllers
@@ -19,6 +21,31 @@ namespace U3ActRegistroDeActividadesApi.Controllers
                 return Ok(deptos);
             }
             return NotFound("No se han encontrado departamentos");
+        }
+
+        //Ejemplo de uso de validadores con fluentvalidation
+        [HttpPost]
+        public IActionResult Agregar(DepartamentoDTO dto)
+        {
+            DepartamentoDTOValidator validador = new();
+            var result = validador.Validate(dto);
+            if (result.IsValid)
+            {
+                return Ok();
+            }
+            return BadRequest();
+        }
+
+        [HttpDelete("/Eliminar/{id:int}")]
+        public IActionResult Eliminar(int id)
+        {
+            var departamento = departamentosRepository.GetById(id);
+            if (departamento != null)
+            {
+                departamentosRepository.Delete(departamento);
+                return Ok();
+            }
+            return NotFound("No se ah encontrado el departamento");
         }
     }
 }
---
+++ U3ActRegistroDeActividadesApi/Models/Validators/DepartamentoDTOValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using U3ActRegistroDeActividadesApi.Models.DTOs;
+
+namespace U3ActRegistroDeActividadesApi.Models.Validators
+{
+    public class DepartamentoDTOValidator : AbstractValidator<DepartamentoDTO>
+    {
+        public DepartamentoDTOValidator()
+        {
+            //Aqui agregas las condiciones que necesitas
+            RuleFor(x => x.Nombre).NotNull().NotEmpty().WithMessage("El nombre del departamento no puede ser nulo o vacio");
+        }
+    }
+
+}
