53189ff85d93f0ff223361205b82f3a36929957c
Luis Roberto Rivera Gonzalez <181G0258@rcarbonifera.tecnm.mx>
2024-05-13 05:51:24 -0600

    prototipo de Update en ActividadesController

--- U3ActRegistroDeActividadesApi/Controllers/ActividadesController.cs
+++ U3ActRegistroDeActividadesApi/Controllers/ActividadesController.cs
@@ -51,7 +51,26 @@ namespace U3ActRegistroDeActividadesApi.Controllers
         [HttpPut("/Update")]
         public IActionResult Editar(ActividadDTO dto)
         {
-            return Ok();
+            ActividadDTOValidator validador = new();
+            var result = validador.Validate(dto);
+            if (result.IsValid)
+            {
+                var actividad = Repositorio.GetActividad(dto.Id);
+                if (actividad != null)
+                {
+                    actividad.Estado = 1;
+                    actividad.FechaActualizacion = DateTime.UtcNow; //fecha en la que se realizo el cambio
+                    actividad.Descripcion = dto.Descripcion;
+                    actividad.Titulo = dto.Titulo;
+                    actividad.FechaRealizacion = dto.FechaRealizacion;
+                    actividad.IdDepartamento = dto.IdDepartamento;
+
+                    Repositorio.Update(actividad);
+                    return Ok("Actividad eliminada");
+                }
+                return NotFound("No existe la actividad que se desea eliminar");
+            }
+            return BadRequest("Selecciona la actividad que desee eliminar");
         }
     }
 }
