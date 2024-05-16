ea0109bd4e2caacb8ad75ed698b867fb1247db4b
VICTOR OSVALDO RODRIGUEZ HIDALGO <201G0283@rcarbonifera.tecnm.mx>
2024-05-16 01:22:43 -0600

    cambios

---
+++ U3ActRegistroDeActividadesMaui/Models/DTOs/ActividadDTO.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U3ActRegistroDeActividadesMaui.Models.DTOs
+{
+    internal class ActividadDTO
+    { 
+        public int Id { get; set; }
+        public string Titulo { get; set; } = null!;
+        public string? Descripcion { get; set; }
+        public DateOnly? FechaRealizacion { get; set; }
+        public int IdDepartamento { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public DateTime FechaActualizacion { get; set; }
+        public int Estado { get; set; }
+        public IFormFile Imagen { get; set; } = null!;
+    }
+}
